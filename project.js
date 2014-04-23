5ed4fa5e50ffa56645c1cbcd46c2c333bd280be0
EWB User <ewb@unassigned-hostname>
2014-04-22 21:12:48 -0400

    enemy chases character health bar depleats

--- project.js
+++ project.js
@@ -7,39 +7,78 @@ var num = 0;
 var playerX = 200;
 var playerY = 330;
 var healthBarX = 200;
-var enemy1Y = 330;
 var enemyX = 750; 
+var enemyY = 330;
+var tempEnemyX = 750;
+var tempEnemyY = 330;
 
 // put functions here, just like kahn academy
 
 var draw = function() {
-
-    //tiled floor
-    fill(255,255,255);
-    for(var numX = 0; numX < 800; numX += 50) {
-	for(var numY = 0; numY < 600; numY += 55) {
-	    rect(numX,numY,tileX,tileY);
-        }
-    }
-
-
-    //player
-    fill(133,21,0);
-    rect(playerX, playerY, 50, 55);
-
-    //health bar
-    fill(255,0,0);
-    rect(10,10,healthBarX ,10);
+	//tiled floor
+	fill(255,255,255);
+	for(var numX = 0; numX < 800; numX += 50) {
+		for(var numY = 0; numY < 600; numY += 55) {
+			rect(numX,numY,tileX,tileY);
+		}
+	 }
 
 
-    //enemy1
-    fill(43,123,0);
-    rect(enemyX,enemy1Y,50,55);
-    enemyX -= 1;
+	//player
+	fill(133,21,0);
+	rect(playerX, playerY, 50, 55);
+	//health bar
+	fill(255,0,0);
+	rect(10,10,healthBarX ,10);
+	if (enemyX === playerX && enemyY === playerY) {
+		healthBarX -= 1;
+	}
+	//enemy1
+	if (playerX > enemyX) {
+		if ((tempEnemyX / 50) % 1 === 0) {
+			enemyX = tempEnemyX;		
+		}
+		fill(43,123,0);
+		rect(enemyX,enemyY,50,55);	
+		tempEnemyX += 1;
+	}
+	if (playerX < enemyX) {
+		if ((tempEnemyX / 50) % 1 === 0) {
+			enemyX = tempEnemyX;		
+		}
+		fill(43,123,0);
+		rect(enemyX,enemyY,50,55);	
+		tempEnemyX -= 1;
+	}
+	if (playerY > enemyY) {
+		if ((tempEnemyY / 55) % 1 === 0) {
+			enemyY = tempEnemyY;		
+		}
+		fill(43,123,0);
+		rect(enemyX,enemyY,50,55);	
+		tempEnemyY += 1;
+	}
+	if (playerY < enemyY) {
+		if ((tempEnemyY / 55) % 1 === 0) {
+			enemyY = tempEnemyY;		
+		}
+		fill(43,123,0);
+		rect(enemyX,enemyY,50,55);	
+		tempEnemyY -= 1;
+	}
+	
+
+	//game over
+	if (healthBarX < 0) {
+		background(0,0,0);
+		fill(255,0,0);
+		textSize(50);
+		text("YOU DIED!!!", 250, 300);
+	}
 }
 
 var keyPressed = function() {
-    if (keyCode === 83) {
+	if (keyCode === 83) {
 	playerY += 55;
     }
     if (keyCode === 87) {
