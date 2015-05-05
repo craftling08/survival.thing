53c7262053d207453935f787afd3b55d12b3c031
EWB User <ewb@unassigned-hostname>
2015-05-05 17:39:14 -0400

    attempted to form blocks

--- project.js
+++ project.js
@@ -2,60 +2,53 @@ title('Break In');
 
 // put variables here, just like kahn academy
 
-var paddleX = 750;
-tiles = [];
-
 //tile constructor
 var Tiles = function (x, y)  {
     this.x = x;
     this.y = y;
     this.width = 50;
-} 
+}; 
 
-//Tiles.prototype.drawNotDestroyed = function() {
-//    fill(255, 0, 0);
-//    strokeWeight(5);
-//    rect(this.x, this.y, this.width, this.width, -10);
-//}
+Tiles.prototype.drawNotDestroyed = function() {
+    fill(255, 0, 0);
+    strokeWeight(5);
+    rect(this.x, this.y, this.width, this.width, 10);
+};
 
-
-
-var num_cols = 5;
-var num_rows = 16;
+var num_cols = 16;
+var num_rows = 5;
 var selected = [];
+var tiles = [];
 
-for(var i = 0; i < num_cols; i++) {
-    for(var j = 0; j < num_rows; j++) {
-        tiles.push(new Tiles(i * 78 + 10, j * 78 + 75, selected.pop()));       
-    }    
-}
-
-for(var i=0; i < tiles.length; i++ ) {
-    tiles[i].drawNotDestroyed();
-}
 
 // put functions here, just like kahn academy
 
-fill(255, 0, 0);
-strokeWeight(5);
-rect(this.x, this.y, this.width, this.width, -10);
 
 // this code is executed once when the program is started
 var setup = function() {
 
     // set up the size of the canvas (you probably don't want to change this!)
     size(800, 600);
-    
-
-    
+      
 };
-
-var draw = function() {
-      background(0, 0, 0);                
-}
-
-
 // override draw function, by default it will be called 60 times per second
 
+var draw = function() {
+     
+    for(var i = 0; i < num_cols; i++) {
+	for(var j = 0; j < num_rows; j * 78 + 10, j * 78 + 75, selected.pop()){
+            
+	}    
+    }
+
+    for(var i = 0; i < tiles.length; i++) {
+	tiles[i].drawNotDestroyed();
+    }
 // call a function (defined up above)
-setup();
+
+};
+
+
+
+
+
