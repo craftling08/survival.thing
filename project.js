1f2b9bff0a176f48857dfe1e5427206fc718a10a
EWB User <ewb@unassigned-hostname>
2015-05-12 16:41:59 -0400

    added blocks, paddle, ball, and lag

--- project.js
+++ project.js
@@ -11,15 +11,20 @@ var Tiles = function (x, y)  {
 
 Tiles.prototype.drawNotDestroyed = function() {
     fill(255, 0, 0);
-    strokeWeight(5);
-    rect(this.x, this.y, this.width, this.width, 10);
+    strokeWeight(2);
+    rect(this.x, this.y, this.width+25, this.width+25);
 };
 
-var num_cols = 16;
-var num_rows = 5;
+var num_cols = 10;
+var num_rows = 3;
 var selected = [];
 var tiles = [];
-
+var paddleX = 300;
+var paddleY = 550;
+var ballX = 100;
+var ballY = 100;
+var ballSpeedX = 5;
+var ballSpeedY = 5;
 
 // put functions here, just like kahn academy
 
@@ -34,19 +39,43 @@ var setup = function() {
 // override draw function, by default it will be called 60 times per second
 
 var draw = function() {
-     
+    background(250, 250, 250);
     for(var i = 0; i < num_cols; i++) {
-	for(var j = 0; j < num_rows; j * 78 + 10, j * 78 + 75, selected.pop()){
-            
+	for(var j = 0; j < num_rows; j++){
+            tiles.push(new Tiles(i * 78 + 10, j * 78 + 75, selected.pop()));   
 	}    
     }
 
     for(var i = 0; i < tiles.length; i++) {
 	tiles[i].drawNotDestroyed();
     }
+
+    //paddle
+    fill(0, 0, 0);
+    rect(paddleX, paddleY, 150, 15);
+
+    if(keyIsPressed) {
+	if(keyCode === LEFT) {
+	    paddleX -= 15;
+	} else if(keyCode === RIGHT) {
+	    paddleX += 15;
+	} 
+    }
+
+    fill(255, 0, 225);
+    strokeWeight(1);
+    ellipse(ballX, ballY, 50, 50);
+    //ballX = ballX + ballSpeedX;
+    //ballY = ballY + ballSpeedY;
+
+    //at left
+    if(ballX < 25) {
+
+    }	
 // call a function (defined up above)
 
 };
+
 
 
 
